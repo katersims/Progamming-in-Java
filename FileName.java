fa050bbc3ddb70adad4ad1afc37a8af30a98a580
Kate <kate.sims@carwow.co.uk>
2018-05-14 08:20:02 +0100

    First commit

---
+++ FileName.java
@@ -0,0 +1,7 @@
+public class FileName {
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+		System.out.println("It is Katie here");
+		System.out.println("Please enter your address below");
+	}
+}
---
+++ KateSims.java
@@ -0,0 +1,33 @@
+public class KateSims {
+	// This is how you write a comment in Java.
+	// There is another way, but we will not cover that now.
+
+	// The computer will ignore the comments. Anything after double
+	// forward slash.
+
+	// public - so everyone can access it 
+	// class - this is a class
+	// KateSims - this is the class name
+	// {this is the opening curly brace of the class
+
+//This is the opening line to all java code at this stage. 
+
+	public static void main (String[] args) {
+		// args - arguments 
+		// public refer to comment on line 5 
+		// static - we'll cover this later
+		// void - the retun type, void means there is no return value 
+		// main - the name of this method 
+		// (String[] args) - arguments to the method. This method must be given in an array of Strings.  
+		// The array will be called "args"
+		// { open braces of the method 
+		System.out.print("I rule!");
+
+
+
+
+	}
+	// The above is the closing brace of the main method
+}
+// The above line is the closing brace of the KateSims class.
+
